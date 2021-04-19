3c5fe1c455211e4a4b8a4ececd418fb0e7675a3d
Quincy Cheng <quincy.cheng@gmail.com>
2021-04-19 17:28:44 +0800

    init version

---
+++ vars/cemGetRemediations.groovy
@@ -0,0 +1,11 @@
+def call(Map config = [:]) {
+   theUrl = 'https://api.cem.cyberark.com/recommendations/remediations?'
+   if (config.platform?.trim()) { theUrl += "&platform=${config.platform}" }
+   if (config.accountId?.trim()) { theUrl += "&account_id=${config.accountId}" }
+   if (config.entityId?.trim()) { theUrl += "&entity_id=${config.entityId}" }
+
+   result = cemGetRequest(url: theUrl, token: cemLogin(config) )
+   println( result )
+
+   return result
+}
