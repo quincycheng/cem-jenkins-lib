704f009ee707b95453a861a717a6e521b1c59263
Quincy Cheng <quincy.cheng@gmail.com>
2021-04-19 15:09:07 +0800

    init version

--- vars/cemLogin.groovy
+++ vars/cemLogin.groovy
@@ -1,3 +1,16 @@
 def call(Map config = [:]) {
-   sh "echo ${config.org} ${config.apiKey}"
+   def requestBody = "{\"organization\": \"${config.org}\","accessKey\":\"${config.apiKey}\"}"
+
+
+  sh "echo ${requestBody}"
+
+  response = httpRequest customHeaders: [[name: 'Content-Type', value: 'application/x-www-form-urlencoded;charset=UTF-8']], 
+		httpMode: "POST",
+  		requestBody:requestBody,
+		url: "https://api.cem.cyberark.com/apis/login"
+             
+
+  sh "echo ${response.content}"
+
+
 }
