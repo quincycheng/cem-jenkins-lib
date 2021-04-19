2863feabe342729903d3904c3482c0b70c1a94af
Quincy Cheng <quincy.cheng@gmail.com>
2021-04-19 15:43:38 +0800

    init version

--- vars/cemLogin.groovy
+++ vars/cemLogin.groovy
@@ -11,9 +11,11 @@ def call(Map config = [:]) {
 
   if (postRC.equals(200)) {
     respText = post.getInputStream().getText()
+    post = null
     //println( (String)respText )
     respJson = readJSON text: (String)respText
     println( respJson.token )
+
   }
 }
 
