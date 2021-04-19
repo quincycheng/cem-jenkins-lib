ebbbb7ac8c580c0d1ac9ea62f971a581900e83ac
Quincy Cheng <quincy.cheng@gmail.com>
2021-04-19 16:28:51 +0800

    init version

--- vars/cemGetRequest.groovy
+++ vars/cemGetRequest.groovy
@@ -1,16 +1,15 @@
 def call(Map config = [:]) {
   def get = new URL(config.url).openConnection()
-
   get.setRequestMethod('GET')
   get.setDoOutput(true)
   get.setRequestProperty('Content-Type', 'application/json')
   get.setRequestProperty('Authorization', "Bearer ${config.token}")
-
   def getRC = get.getResponseCode()
 
   if (getRC.equals(200)) {
     respText = get.getInputStream().getText()
     get = null
-    return readJSON text: (String)respText
+    respJson =  readJSON text: (String)respText
+    return respJson
   }
 }
