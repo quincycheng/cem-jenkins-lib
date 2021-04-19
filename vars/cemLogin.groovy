c783589acffb9c84de2d5768ead575b0e4214643
Quincy Cheng <quincy.cheng@gmail.com>
2021-04-19 15:37:39 +0800

    init version

--- vars/cemLogin.groovy
+++ vars/cemLogin.groovy
@@ -1,21 +1,21 @@
 def call(Map config = [:]) {
   def reqBody = "{\"organization\": \"${config.org}\",\"accessKey\":\"${config.apiKey}\"}"
-  def reqUrl = "https://api.cem.cyberark.com/apis/login"
+  def reqUrl = 'https://api.cem.cyberark.com/apis/login'
 
-  def post = new URL(reqUrl).openConnection();
-  post.setRequestMethod("POST")
+  def post = new URL(reqUrl).openConnection()
+  post.setRequestMethod('POST')
   post.setDoOutput(true)
-  post.setRequestProperty("Content-Type", "application/json")
-  post.getOutputStream().write(reqBody.getBytes("UTF-8"));
-  def postRC = post.getResponseCode();
+  post.setRequestProperty('Content-Type', 'application/json')
+  post.getOutputStream().write(reqBody.getBytes('UTF-8'))
+  def postRC = post.getResponseCode()
 
-  if(postRC.equals(200)) {
-    respText = post.getInputStream().getText() 
-    println( respText );
+  if (postRC.equals(200)) {
+    respText = post.getInputStream().getText()
+    println( respText )
 
-    //def resultJson = readJSON text: post.getInputStream().getText();
-    //println( resultJson.token )
+    respJson = readJSON text:respText
+
+    println( respJson.token )
   }
-
 
 }
