5a9a053064c2de23d239555eb378a9c75bf22dc3
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-03 19:43:31 +0800

    Update cemGetAccounts.groovy

--- vars/cemGetAccounts.groovy
+++ vars/cemGetAccounts.groovy
@@ -1,6 +1,8 @@
 def call(Map config = [:]) {
    theUrl = 'https://api.cem.cyberark.com/customer/platforms/accounts'
 
+   println( "debug " + cemLogin(config) )
+   
    result = cemGetRequest(url: theUrl, token: cemLogin(config) )
    return result
 }
