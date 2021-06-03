5d617e1afbf892f7d09ac2b1088fa99c5c3340ea
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-03 20:48:30 +0800

    Update cemLogin.groovy

--- vars/cemLogin.groovy
+++ vars/cemLogin.groovy
@@ -16,6 +16,9 @@ def call(Map config = [:]) {
   post.setRequestProperty('Content-Type', 'application/json')
   post.setRequestProperty("Accept", "*/*");
   post.setRequestProperty('User-Agent', 'PostmanRuntime/7.28.0')
+  
+  post.setRequestProperty('Content-Length', Integer.toString( reqBody.getBytes('UTF-8').length ))
+
  // post.setRequestProperty('Host', 'api.cem.cyberark.com')
 
   //println("debug - login api content length: " + post.getContentLength() )
