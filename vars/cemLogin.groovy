0de5934308bf262728cc35e5b277be7870318476
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-03 20:41:56 +0800

    Update cemLogin.groovy

--- vars/cemLogin.groovy
+++ vars/cemLogin.groovy
@@ -19,7 +19,7 @@ def call(Map config = [:]) {
   post.setRequestProperty('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0')
   post.setRequestProperty('Host', 'api.cem.cyberark.com')
 
-    println("debug - login api content length: " + post.getContentLength() )
+  //println("debug - login api content length: " + post.getContentLength() )
 
   post.setRequestProperty('Content-Length', Integer.toString( post.getContentLength() ))
 
