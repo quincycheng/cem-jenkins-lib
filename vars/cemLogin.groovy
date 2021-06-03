102c63dcb7323ef88adc8f8015cabe28e996eff9
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-03 21:54:04 +0800

    Update cemLogin.groovy

--- vars/cemLogin.groovy
+++ vars/cemLogin.groovy
@@ -16,8 +16,8 @@ def call(Map config = [:]) {
   post.setDoInput(true);
 
   post.setRequestProperty('Content-Type', 'application/json')
-  //post.setRequestProperty("Accept", '*/*');
-  post.setRequestProperty('User-Agent', 'PostmanRuntime/7.28.0')
+  post.setRequestProperty("Accept", '*/*');
+  post.setRequestProperty('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36')
   post.setRequestProperty('Host', 'api.cem.cyberark.com')
   post.setRequestProperty('Content-Length', Integer.toString( reqBody.getBytes('UTF-8').length ) )
 
